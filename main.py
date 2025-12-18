d10323bcbdd2d569a41100a4451059b393fc3850
Ganesh Ghumade <ganeshghumade20062006@gmail.com>
2025-12-18 07:11:46 +0530

    Basics Of Python
    Some  basics  concept  of  python

--- main.py
+++ main.py
@@ -37,3 +37,21 @@ print("today is holiday")
 4
 5
 # ..........
+
+# Basic of python
+# Today I will write my first ever python program from scratch. It will consist of a bunch of print statements. print can be used to print something on the console in python
+
+print("hello world", 44)
+# OUTPUT= hello world 44
+print(4)
+# OUTPUT= 4
+print(455*444)
+# OUTPUT= 202020
+print(55+62)
+# OUTPUT= 117
+1
+2
+3
+4
+5
+# ..........
