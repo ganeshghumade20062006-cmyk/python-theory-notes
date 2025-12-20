1c87a13da484ecb0900a4e15abe2eff8dc51b7cd
Ganesh Ghumade <ganeshghumade20062006@gmail.com>
2025-12-20 08:50:00 +0530

    Variables and Data Types
    1.What is a variable
    2.What is a Data Type:
    Numeric data: int, float, complex
    Text data: str
    Boolean data
    Sequenced data: list, tuple
    Mapped data: dict

--- main.py
+++ main.py
@@ -136,3 +136,102 @@ print("my moobile number is", 8308606736, sep="~", end="458")
 4
 5
 # ..........
+
+# Variables and Data Types
+
+
+# What is a variable?
+# Variable is like a container that holds data. Very similar to how our containers in kitchen holds sugar, salt etc Creating a variable is like creating a placeholder in memory and assigning it some value. In Python its as easy as writing:
+a = 1
+b = True
+c = "Harry"
+d = None
+# These are four variables of different data types.
+
+
+# What is a Data Type?
+# Data type specifies the type of value a variable holds. This is required in programming to do various operations without causing an error.
+# In python, we can print the type of any operator using type function:
+
+a = 1
+print(type(a))
+b = "1"
+print(type(b))
+
+# By default, python provides the following built-in data types:
+# 1. Numeric data: int, float, complex
+# int: 3, -8, 0
+# float: 7.349, -9.0, 0.0000001
+# complex: 6 + 2i
+# 2. Text data: str
+# str: "Hello World!!!", "Python Programming"
+
+# 3. Boolean data:
+# Boolean data consists of values True or False.
+
+# 4. Sequenced data: list, tuple
+# list: A list is an ordered collection of data with elements separated by a comma and enclosed within square brackets. Lists are mutable and can be modified after creation.
+
+# example:
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+# Output:
+
+# [8, 2.3, [-4, 5], ['apple', 'banana']]
+
+# Tuple: A tuple is an ordered collection of data with elements separated by a comma and enclosed within parentheses. Tuples are immutable and can not be modified after creation.
+
+# Example:
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+# Output:
+
+# (('parrot', 'sparrow'), ('Lion', 'Tiger'))
+
+# 5. Mapped data: dict
+# dict: A dictionary is an unordered collection of data containing a key:value pair. The key:value pairs are enclosed within curly brackets.
+
+# Example:
+
+dict1 = {"name":"Harshal", "age":20, "canVote":True}
+print(dict1)
+
+# Output:
+
+# {'name': 'Harshal', 'age': 20, 'canVote': True}
+
+
+# some example of it
+a="ganesh ghumade"
+b=1556
+c="tigar"
+d="none"
+e="True"
+f="false"
+print("the type of a is",(a))
+# output= the type of a is ganesh ghumade
+print("the type of b is", type(b))
+# outut= the type of b is <class 'int'>
+print("the type of c is", type (c))
+# output= the type of c is <class 'str'>
+print("the type of d is", type(d))
+# output= the type of d is <class 'str'>
+print("the type of e is", type(e))
+# output= the type of e is <class 'str'>
+print("the type of f is", type(f))
+# output= the type of f is <class 'str'>
+s=complex(45,5)
+# output= the type of s is <class 'complex'>
+print("the type of s is", type(s))
+# output= the type of s is <class 'complex'>
+1
+2
+3
+4
+5
+#..........
+
