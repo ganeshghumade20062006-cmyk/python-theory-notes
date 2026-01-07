3aeb89af301e4367da853125dedf1a8a2161520b
Ganesh Ghumade <ganeshghumade20062006@gmail.com>
2026-01-07 09:34:02 +0530

    Python Functions
    1.Built-in functions
    2.User-defined functions
    3.User-defined functions
    4.Calling a function

--- main.py
+++ main.py
@@ -1549,3 +1549,125 @@ while True:
 5
 # ..........
 
+# Python Functions
+# replit=Python Functions
+# A function is a block of code that performs a specific task whenever it is called. In bigger programs, where we have large amounts of code, it is advisable to create or use existing functions that make the program flow organized and neat.
+
+# There are two types of functions:
+
+# Built-in functions
+# User-defined functions
+# Built-in functions:
+# These functions are defined and pre-coded in python. Some examples of built-in functions are as follows:
+
+# min(), max(), len(), sum(), type(), range(), dict(), list(), tuple(), set(), print(), etc.
+
+# User-defined functions:
+# We can create functions to perform specific tasks as per our needs. Such functions are called user-defined functions.
+
+# Syntax:
+# def function_name(parameters):
+#   pass
+#   # Code and Statements
+
+# Create a function using the def keyword, followed by a function name, followed by a paranthesis (()) and a colon(:).
+# Any parameters and arguments should be placed within the parentheses.
+# Rules to naming function are similar to that of naming variables.
+# Any statements and other code within the function should be indented.
+# Calling a function:
+# We call a function by giving the function name, followed by parameters (if any) in the parenthesis.
+
+# Example:
+
+# def name(fname, lname):
+#     print("Hello,", fname, lname)
+# name("Sam", "Wilson")
+
+# Output:
+
+# Hello, Sam Wilson
+
+
+
+
+# examples
+
+a=9
+b=8
+gmean1=(a*b)/(a+b)
+print(gmean1)
+
+# outpur= 4.235294117647059
+
+c=8
+d=7
+gmean2= (c*d)/(c+d)
+print(gmean2)
+
+# output= 3.7333333333333334
+
+# python functios starts from here
+def calculateGmean(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+a=9
+b=8
+if(a>b):
+    print("first number is grater")
+else:
+    print("second number is greater or equal")    
+
+
+# # output= first number is grater
+
+a=8
+b=9
+if(a>b):
+    print("first number is grater")
+else:
+    print("second number is greater or equal")
+    
+# # output second number is greater or equal
+
+
+
+
+
+def calculateGmean(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+
+a=9
+b=8
+if(a>b):
+    print("first number is grater")
+else:
+    print("second number is greater or equal") 
+
+calculateGmean(a,b)
+c=8
+d=74
+if(c>d):   
+    print("first number is grater")
+else:
+    print("second number is grater or equal")
+
+calculateGmean(c,d)      
+
+
+
+# # output= 
+# first number is grater
+# 4.235294117647059
+# second number is grater or equal
+# 7.219512195121951
+
+
+# output right.. self study of this topic
+1
+2
+3
+4
+5
+# ..........
+
